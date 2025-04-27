fe309185411a09c23c7095aa3e82a17c60fe86c8
sjimenezn <72929512+sjimenezn@users.noreply.github.com>
2025-04-26 20:44:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -80,5 +80,5 @@ def home():
 
 if __name__ == "__main__":
     threading.Thread(target=monitor_growatt, daemon=True).start()
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=8000)
 
